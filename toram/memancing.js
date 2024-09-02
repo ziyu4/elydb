2f21150a91d07d5070741ea0e10d0b9856f002ea
Pandu <144518692+arasea2@users.noreply.github.com>
2024-09-02 17:12:49 +0700

    Create memancing.js

---
+++ toram/memancing.js
@@ -0,0 +1,23 @@
+Tutorial Memancing
+- Buat perabot Meja Peralatan Mancing
+- Buat joran di Meja Peralatan Mancing
+- Atur shortcut memancing
+- Pilih spot memancing didekat air
+
+Spot Mancing :
+- Sofya
+- Basin Korda
+- Reruntuhan Athema
+- Hora diomeda
+- Reservoir Copia
+- Jalan Wazeero
+- Nov Saterica
+- Lahan Basah Nezim
+- Tepian Air Anugerah
+- Labirin Serikat
+
+Saran Joran Pancing (Carilah Stats di antara 80-100)
+- Logam (500) - Kayu (500)
+- Fauna (500) - Kayu (500)
+- Logam + Fauna (500) + Kayu (500)
+- Stat dihitung 10% - 20% dari material +1 (mats x 10-20% + 1
